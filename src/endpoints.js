16dd22e6c128a8a46784c157c1555fa9e88dac2f
Shea Scott <sheadscott@gmail.com>
2024-05-14 14:40:07 -0500

    Added api endpoints to /src/endpoints.js

---
+++ src/endpoints.js
@@ -0,0 +1,9 @@
+const endpoint = 'https://instruction.austincc.edu/tled/wp-json/';
+
+export const dropdownData = `${endpoint}wp-api-menus/v2/menus/4`;
+
+export const homeData = `${endpoint}wp/v2/pages?slug=new-home`;
+
+export const topnavData = `${endpoint}wp-api-menus/v2/menus/3`;
+
+export const pageContent = `${endpoint}wp/v2/pages?slug=`;
