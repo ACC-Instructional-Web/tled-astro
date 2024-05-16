d1bdad89d5b0b2462246588941c86eec3ec48470
Shea Scott <sheadscott@gmail.com>
2024-05-16 16:07:19 -0500

    EventsList + Homepage scaffolding

--- src/endpoints.js
+++ src/endpoints.js
@@ -9,3 +9,6 @@ export const topnavData = `${endpoint}wp-api-menus/v2/menus/3`;
 export const pageContent = `${endpoint}wp/v2/pages?slug=`;
 
 export const carouselData = `${endpoint}wp/v2/pages?slug=home`;
+
+export const eventsSheet =
+  'https://sheets.googleapis.com/v4/spreadsheets/1feRPiGXCX19MZofZ2LgLgYbvxpGJYFByhJ6ZAlOkWiM/values/CurrentEvents?key=AIzaSyBJU4PeKulVbFBMrGUm9lSN3FwIdTQZQaE';
