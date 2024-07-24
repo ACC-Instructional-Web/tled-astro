e9532333bd373c6dbd63ca2ef9e97b164ec2e94a
Shea Scott <sheadscott@gmail.com>
2024-07-24 11:31:56 -0500

    Cleanup for SST deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,6 +27,7 @@ const whenExternalScripts = (items = []) =>
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  // output: 'server',
   output: 'hybrid',
   adapter: aws(),
 
