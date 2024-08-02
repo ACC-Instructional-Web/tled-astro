1c37f12ad395f853cd7e9c0a769ac405717a1a82
Shea Scott <sheadscott@gmail.com>
2024-08-02 14:08:24 -0500

    Search and Events Calendar

---
+++ src/components/widgets/Calendar/CalendarGrid.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import Event from './Event.jsx';
+
+const getUniqueTags = (events) => {
+  // Get a unique array of tags
+  let allTags = [];
+  events.forEach(function (item) {
+    if (item[8]) {
+      const tagArray = item[8].trim().split(/\s+/);
+      allTags = [...allTags, ...tagArray];
+    }
+  });
+  // Convert the array to unique values
+  allTags = [...new Set(allTags)].sort();
+  return allTags;
+};
+
+const filterEvents = (events) => {
+  events
+    .filter((event) => new Date(event[0].replace(/.*?,\s/, '')).valueOf() > Date.now())
+    .sort((a, b) => {
+      // Take the day of the week off the front of the string and convert to Date # value
+      // Probably unnecessary since the events are returned in the order in sheet
+      a = new Date(a[0].replace(/.*?,\s/, '')).valueOf();
+      b = new Date(b[0].replace(/.*?,\s/, '')).valueOf();
+      return a - b;
+    });
+};
+
+export default function CalendarGrid({ calendarData }) {
+  // const [tags, setTags] = useState([]);
+  const [activeTags, setActiveTags] = useState([]);
+  const [activeEvents, setActiveEvents] = useState([]);
+
+  const toggleTag = (tag) => {
+    console.log('TAG: ', tag);
+    // Make a copy of current state activeTags
+    const at = [...activeTags];
+    // console.log(activeTags);
+    // Add or remove tag from activeTags
+    at.includes(tag) ? at.splice(at.indexOf(tag), 1) : at.push(tag);
+
+    // this.filterSessionsByTag(at);
+
+    // If there are active tags, filter the events by tag else show all events
+    at.length > 0 ? setActiveEvents(filterEventsByTag(at)) : setActiveEvents(events);
+    console.log('ActiveTags :>> ', at);
+    console.log('activeEvents :>> ', activeEvents);
+    setActiveTags(at);
+  };
+
+  const filterEventsByTag = (activeTags) =>
+    events.filter((event) => {
+      const eventTags = event[8].trim().split(/\s+/);
+      return (
+        // return true if event has at least one active tag
+        activeTags.some((tag) => eventTags.indexOf(tag) > -1)
+      );
+    });
+
+  const events = calendarData.values;
+
+  // Remove the header row
+  events.shift();
+
+  filterEvents(events);
+
+  const tags = getUniqueTags(events);
+  // setat(tags);
+
+  return (
+    <div>
+      {tags.length > 0 && (
+        <div className="mt-6 mb-2.5">
+          <p>Click on the tags below to filter the workshops by topic:</p>
+          {tags.map((tag, i) => (
+            <button
+              className="inline-block text-sm active:bg-slate-200 mx-1 bg-slate-100 py-0.5 px-1.5"
+              key={i}
+              onClick={() => toggleTag(tag)}
+              // active={activeTags.includes(tag)}
+            >
+              {tag}
+            </button>
+          ))}
+        </div>
+      )}
+
+      <div className="flex flex-wrap justify-around -mx-4">
+        {activeEvents.length > 0
+          ? activeEvents.map((event, i) => <Event event={event} key={i} />)
+          : events.map((event, i) => <Event event={event} key={i} />)}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/widgets/Calendar/Event.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+// [
+//   "Date for Auto Check",
+//   "Web Display Date",
+//   "Event Title",
+//   "Presenters",
+//   "Mode",
+//   "Image",
+//   "Link to Register",
+//   "Flag",
+//   "Tags",
+//   "Start Time Formatted",
+//   "End Time Formatted (leave blank if all day)"
+// ]
+
+const Event = (props) => {
+  const { event } = props;
+  const tags = event[8] ? event[8].trim().split(/\s+/) : null;
+  const bgImg = event[5];
+  const title = event[2];
+  const link = event[6];
+  return (
+    <div className="min-w-40 m-4 cursor-pointer basis-1/4">
+      <div style={{ backgroundImage: `url(${bgImg})` }} className="bg-cover h-0 w-full flex pb-[100%] mb-4">
+        {event[7] && <span>{event[7]}</span>}
+      </div>
+      {event[1]}
+      <h1>{link ? <a href={link}>{title}</a> : <span>{title}</span>}</h1>
+      {tags && (
+        <div>
+          {tags.map((tag) => (
+            <div className="inline-block py-0.5 px-1.5 m-1 text-sm" key={tag}>
+              {tag}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Event;
+
+// const EventBox = styled.div`
+//   /* flex: 1 1 200px; */
+//   flex: 0 0 20%;
+//   min-width: 170px;
+//   margin: 1rem;
+//   cursor: pointer;
+//   transition: all 0.1s;
+
+//   @media (max-width: 768px) {
+//     min-width: 240px;
+//   }
+
+//   /* &:last-child {
+//     @media (max-width: 619px) {
+//       max-width: 100%;
+//     }
+//     max-width: 280px;
+//   } */
+
+//   &:hover {
+//     transform: scale(0.99);
+//   }
+
+//   h1 {
+//     // color: blue;
+//     font-size: 1rem;
+//   }
+// `;
+
+// const Image = styled.div`
+//   height: 0;
+//   padding-bottom: 100%;
+//   width: 100%;
+//   // background: url('https://drive.google.com/thumbnail?id=1bvC4SRreXx3SKsBoj3ui6EphifhAB_dm&sz=w400') no-repeat top left;
+//   background: ${(props) => `url(${`${props.image}&sz=w400`}) no-repeat top left`};
+//   background-size: cover;
+//   display: flex;
+//   margin-bottom: 1rem;
+// `;
+
+// const Flag = styled.span`
+//   background-color: #000;
+//   color: #fff;
+//   font-size: 0.9rem;
+//   display: inline-block;
+//   text-align: center;
+//   padding: 0.6rem 1rem;
+//   margin-left: auto;
+//   margin-top: 1rem;
+//   height: fit-content;
+//   text-transform: uppercase;
+// `;
+
+// const Tag = styled.span`
+//   display: inline-block;
+//   padding: 2px 6px;
+//   margin: 4px;
+//   background: #efefef;
+//   color: #333;
+//   border-radius: 4px;
+//   font-size: 0.8em;
+// `;
